629784dd7c0f4dde5be1f81a3d42c05d75222788
aulianita31 <aulianita173@gmail.com>
2024-04-18 21:15:10 +0700

    Belajar html TKJ30

---
+++ 2402227js.js
@@ -0,0 +1,13 @@
+var n = prompt("Masukan Nama Anda")
+var c = confirm("Haii "+n+" Semoga kabar baik untuk anda di hari yang cerah ini?");
+if (c==true) {
+    alert ('Allhamdulilah');
+} else {
+    alert ('Ada apa?');
+} {
+    alert ('Semoga bahgia ya!')
+} {
+    alert ('Jangan lupa makan')
+} {
+    alert ('Jangan lupa awali harimu dengan Bismillah')
+}
